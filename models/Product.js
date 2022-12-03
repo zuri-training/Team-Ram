98cb910fa8076295401fad008ff998587f20ec03
ayodev <ayodele466@gmail.com>
2022-12-03 07:46:58 +0100

    Price compare data schema

---
+++ models/Product.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+
+// Original sneaker data will not be stored by us, the API will take care of that.
+// However we will have to store the comments under each sneaker.
+// To do that, we present the following schemas.
+
+const productSchema = mongoose.Schema({
+  // This schema will allow us to save comments per unique sneaker _id.
+  // Duplicate items get their own comment section.
+  // A server request made renders this data.
+  sneaks_id: { type: String, required: true, unique },
+  product_comment: {
+    type: [commentSchema],
+  },
+});
+
+// A comments must be related to a particular user.
+const commentSchema = mongoose.Schema(
+  {
+    user: {
+      _id: { type: String, required: true, unique },
+      username: { type: String, required: true },
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamp: true }
+);
---
+++ models/User.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+    username:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    email:{
+        type:String,
+        require:true,
+        unique:true
+    },
+    password:{
+        type:String,
+        required:true
+    },
+},{timestamps: true});
+
+module.exports = mongoose.model("User", UserSchema);
