88b5d3d3bc14d2a1419709828d8dd94fd060c68f
Tobani <tobanigeorge@gmail.com>
2022-12-09 21:07:33 +0100

    configured product endpoints

---
+++ server/controller/productController.js
@@ -0,0 +1,123 @@
+const Sneaks_API = require("sneaks-api");
+const sneaks = new Sneaks_API();
+const Products = require("../models/Product");
+
+
+// get popular shoes
+exports.getPopular = async (req, res) => {
+  try {
+    sneaks.getMostPopular(4, (err, products) => {
+      if (err) throw err;
+      res.status(200).send({
+        success: true,
+        message: "products found",
+        products,
+      })
+    })
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "error processing request",
+      error,
+    })
+  }
+}
+
+
+// search shoes
+// get many shoes with query
+exports.getProducts = async (req, res) => {
+  try {
+    // a req query containing a search query
+    const { q } = await req.query;
+    sneaks.getProducts(q, 10, (err, products) => {
+      if (err) throw err;
+      res.status(200).send({
+        success: true,
+        message: "correctly processed request",
+        products,
+      });
+    })
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({
+      success: false,
+      message: "error processing request",
+      error,
+    })
+  }
+}
+
+exports.getProductComments = async (req, res) => {
+  try {
+    const { id } = await req.query;
+    Products.findOne({ sneaks_id: id })
+      // execute if it has been saved previously
+      .then(details => {
+        if (!details) throw new Error("create reference first")
+        res.status(200).send({
+          success: true,
+          message: "success getting product details",
+          details,
+        })
+      })
+      // execute if it hasn't been saved previously
+      .catch(err => {
+        if (!id) throw err;
+        const details = new Products({
+          sneaks_id: id,
+          product_comments: [],
+        })
+        details.save((err, details) => {
+          if (err) throw err;
+          res.status(200).send({
+            success: true,
+            message: "saved product in db",
+            details,
+          })
+        });
+      })
+
+  } catch (error) {
+    console.error(error)
+    res.status(500).send({
+      success: false,
+      message: "error processing request",
+      error,
+    })
+  }
+}
+
+exports.addProductComment = async (req, res) => {
+  try {
+    const { id } = await req.query;
+    const { comment } = await req.body;
+    Products.findOne({ sneaks_id: id })
+      .then(product => {
+        if (!product) throw new Error("error getting product");
+        product.product_comments.push(comment);
+        product.save((err, product) => {
+          if (err) throw err;
+          res.status(200).send({
+            success: true,
+            message: "comment added",
+            details: product
+          })
+        })
+      })
+      .catch(e => {
+        res.status(404).send({
+          success: false,
+          message: "an error occured",
+          error: e
+        })
+      })
+  } catch (error) {
+    res.status(500).send({
+      success: false,
+      message: "error processing request",
+      error,
+    })
+  }
+}
--- server/models/Product.js
+++ server/models/Product.js
@@ -4,22 +4,14 @@ const mongoose = require("mongoose");
 // However we will have to store the comments under each sneaker.
 // To do that, we present the following schemas.
 
-const productSchema = mongoose.Schema({
-  // This schema will allow us to save comments per unique sneaker _id.
-  // Duplicate items get their own comment section.
-  // A server request made renders this data.
-  sneaks_id: { type: String, required: true, unique: true },
-  product_comment: {
-    type: [commentSchema],
-  },
-});
-
 // A comments must be related to a particular user.
 const commentSchema = mongoose.Schema(
   {
     user: {
-      _id: { type: String, required: true, unique: true },
-      username: { type: String, required: true },
+      type: {
+        _id: { type: String, required: true, unique: true },
+        name: { type: String, required: true },
+      },
       required: true,
     },
     content: {
@@ -29,3 +21,17 @@ const commentSchema = mongoose.Schema(
   },
   { timestamp: true }
 );
+
+const productSchema = mongoose.Schema({
+  // This schema will allow us to save comments per unique sneaker _id.
+  // Duplicate items get their own comment section.
+  // A server request made renders this data.
+  sneaks_id: { type: String, required: true, unique: true },
+  product_comments: {
+    type: [commentSchema],
+  },
+});
+
+
+const productModel = mongoose.model("products", productSchema);
+module.exports = productModel;
