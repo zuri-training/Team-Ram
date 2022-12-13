ef957e9e29a0a20786663edd28785048bb15db7a
Tobani <tobanigeorge@gmail.com>
2022-12-13 07:10:31 +0100

    login/sign-up functional

---
+++ FrontEnd/scripts/userLogin.js
@@ -0,0 +1,55 @@
+import { API_URL } from "./exports.js";
+
+const loginForm = document.getElementById("login-form");
+
+loginForm.onsubmit = (e) => {
+  // prevent default form behaviour
+  e.preventDefault();
+  // destructure the value property from the input elements
+  const { value: email } = document.getElementById("email");
+  const { value: password } = document.getElementById("psw");
+
+  // form the user object for the backend
+  const user = { email, password };
+  Login(user)
+    // redirect on success
+    .then((res) => {
+      if (!res.success) {
+        throw new Error(res.message);
+      }
+      alert(`Logged in as ${res.user.name}`)
+      location.pathname = "/";
+    })
+    // show error on failure
+    .catch((e) => {
+      console.error(e);
+      showLoginError();
+    })
+}
+
+// send api request
+async function Login(data) {
+  const reqInit = {
+    method: "POST",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+    mode: "cors",
+    body: JSON.stringify({ user: data })
+  }
+  // api endpoint for logins
+  const reqUrl = `${API_URL}/auth/login`;
+  const req = await fetch(reqUrl, reqInit).then(res => res.json()).catch(e => console.error(e));
+  return req;
+}
+
+
+// show error to user
+function showLoginError() {
+  const messageEl = document.getElementById("message");
+  if (messageEl) {
+    messageEl.textContent = "Email or Password is incorrect";
+    messageEl.style.color = "red";
+  }
+}
---
+++ FrontEnd/scripts/userSignup.js
@@ -0,0 +1,58 @@
+import { API_URL } from "./exports.js";
+
+const signUpForm = document.getElementById("sign-up");
+const messageEl = document.getElementById("message");
+
+signUpForm.onsubmit = (e) => {
+  // prevent the default submit behaviour
+  e.preventDefault()
+  const { value: name } = document.getElementById("name");
+  const { value: email } = document.getElementById("email");
+  const { value: password } = document.getElementById("psw");
+  const { value: confirmPassword } = document.getElementById("psw-repeat");
+
+  if (password !== confirmPassword) {
+    return showConsistencyError();
+  }
+
+  const user = { name, email, password };
+
+  SignUp(user)
+    // redirect on success
+    .then((res) => {
+      if (!res.success) throw new Error(res.message);
+      alert(`Signed Up as ${res.user.name}`)
+      location.pathname = "/";
+    })
+    // show error on failure
+    .catch(e => {
+      console.error(e);
+      showEmailError();
+    })
+}
+
+function showConsistencyError() {
+  messageEl.textContent = "Passwords don't match";
+  messageEl.style.color = "red";
+}
+
+async function SignUp(data) {
+  const reqInit = {
+    method: "POST",
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Content-Type": "application/json"
+    },
+    mode: "cors",
+    body: JSON.stringify({ user: data })
+  }
+  // api endpoint for logins
+  const reqUrl = `${API_URL}/auth/sign_up`;
+  const req = await fetch(reqUrl, reqInit).then(res => res.json()).catch(e => console.error(e));
+  return req;
+}
+
+function showEmailError() {
+  messageEl.textContent = "Email is already in use";
+  messageEl.style.color = "red";
+}
