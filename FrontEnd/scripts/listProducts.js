a18b2da194541dec8891cfe7b3c9e8386c8faaf9
Tobani <tobanigeorge@gmail.com>
2022-12-13 22:57:44 +0100

    added api data load for product list page

--- FrontEnd/scripts/listProducts.js
+++ FrontEnd/scripts/listProducts.js
@@ -3,7 +3,6 @@ import { API_URL } from "./exports.js";
 async function shoeSearch() {
   const query = location.search.substring(1);
   const reqQuery = encodeURI(query);
-
   const reqUrl = `${API_URL}/products/search?${reqQuery}`;
 
   const request = await fetch(reqUrl, {
@@ -12,10 +11,49 @@ async function shoeSearch() {
       "Access-Control-Allow-Origin": "*"
     },
     mode: "cors"
-  }).then(res => res.json()).then(value => value.products).catch(e => console.error(e));
-  console.log({ request });
+  })
+    .then(res => res.json())
+    .then(value => value.products)
+    .catch(e => console.error(e));
+
   return request;
 }
 
-const products = shoeSearch();
 
+window.onload = async () => {
+  // call the api
+  const products = await shoeSearch();
+  // create an object containing all the cards
+  const productCards = Object.fromEntries(
+    document.querySelectorAll(
+      ".product-card"
+    ).entries()
+  )
+
+  for (let i in productCards) {
+    const card = productCards[i];
+    const product = products[i];
+
+    // destructure the parts of the card 
+    // and give them their respective props
+    const [image, body] = card.children;
+    image.src = product.thumbnail;
+    image.alt = product.shoeName;
+    image.title = product.shoeName;
+
+    const [name, , end] = body.children;
+    // prevent overly long product names
+    let shoeName = trimName(product.shoeName);
+    name.textContent = shoeName;
+    const [price, link] = end.children;
+    price.textContent = `$${product.retailPrice}`;
+    link.href = `/pages/productDetails.html?id=${product.styleID}`
+  }
+}
+
+function trimName(name) {
+  if (name.length <= 20) return name;
+  // remove extra words and add ellipses
+  name = name.slice(0, 20) + "...";
+  return name;
+}
