fbfc83191d5c068c3fab66bb288ffcda028a7b08
Tobani <tobanigeorge@gmail.com>
2022-12-15 07:57:47 +0100

    removed from secrets being returned to the user

--- server/controller/authController.js
+++ server/controller/authController.js
@@ -66,7 +66,10 @@ exports.signUp = async (req, res) => {
       res.status(201).send({
         success: true,
         message: "created user",
-        user: doc
+        user: {
+		  _id: doc._id,
+		  name: doc.name
+		}
       })
     })
       .catch(e => {
